15668525ad5ed4199fe47dcd39a6cce879bfb422
Agustinus Yudhistira <16520107@std.stei.itb.ac.id>
2021-09-05 22:57:30 +0700

    first commit

---
+++ Code/test.js
@@ -0,0 +1,14 @@
+// new Typed('#typedName',{
+//   strings : ['Agustinus Yudhistira'],
+//   typeSpeed : 200,
+//   delaySpeed : 100,
+//   loop : true
+// });
+
+
+new Typed('#typed',{
+  strings : ['Student','Learner', 'Tech-Enthusiast'],
+  typeSpeed : 100,
+  delaySpeed : 100,
+  loop : true
+});
