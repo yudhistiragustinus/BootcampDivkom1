a2da2ec19e6e96a349340822d3f38d0b7b8459ea
Agustinus Yudhistira <16520107@std.stei.itb.ac.id>
2021-09-06 09:53:03 +0700

    add animation and styling

--- Code/test.js
+++ Code/test.js
@@ -1,14 +1,14 @@
-// new Typed('#typedName',{
-//   strings : ['Agustinus Yudhistira'],
-//   typeSpeed : 200,
-//   delaySpeed : 100,
-//   loop : true
-// });
-
-
 new Typed('#typed',{
   strings : ['Student','Learner', 'Tech-Enthusiast'],
   typeSpeed : 100,
   delaySpeed : 100,
+  delay : 200,
   loop : true
+});
+
+// Aos Library
+AOS.init({
+  offset: 500, // offset (in px) from the original trigger point
+  delay: 150, // values from 0 to 3000, with step 50ms
+  duration: 1500 // values from 0 to 3000, with step 50ms
 });
