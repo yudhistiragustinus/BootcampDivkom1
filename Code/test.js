3dbdf8ea33d03b8dea891251b113ecce90a12217
Agustinus Yudhistira <16520107@std.stei.itb.ac.id>
2021-09-06 09:58:52 +0700

    add more styling

--- Code/test.js
+++ Code/test.js
@@ -8,7 +8,7 @@ new Typed('#typed',{
 
 // Aos Library
 AOS.init({
-  offset: 500, // offset (in px) from the original trigger point
+  offset: 400, // offset (in px) from the original trigger point
   delay: 150, // values from 0 to 3000, with step 50ms
   duration: 1500 // values from 0 to 3000, with step 50ms
 });
